81bb1bebfadc777c754474f61b921d9d1d876832
I am naveen Gullipalli <naveengullipalli317@gmail.com>
2021-07-19 21:56:18 +0200

    Canvas

--- canvas.js
+++ canvas.js
@@ -1,19 +1,55 @@
-window.addEventListener('load', () => {
-    const canvas = document.querySelector("#canvas");
-    const context = canvas.getContext("2d");
-    // re sizing
-   canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+{/* <script src="https://cdn.jsdelivr.net/npm/ef-infinite-canvas@0.5.0-alpha/dist/infinite-canvas.js"></script> */}
 
-     /*  context.beginPath();
+/* window.addEventListener('load', () => { */
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+   
+    // re sizing
+
+    window.addEventListener("resize", function() {
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        
+    });
+    const mouse = {
+        x:null,
+        y:null,
+    }
+    canvas.addEventListener("click", function(event){
+        mouse.x = undefined;
+        mouse.y = undefined;
+        drawCircle();
+    })
+    canvas.addEventListener("mousemove", function(event){
+        mouse.x = event.x;
+        mouse.y = event.y;
+        drawCircle();
+    })
+    function drawCircle() {
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    context.fill();
+    }
+    
+    function animate(){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawCircle();
+        requestAnimationFrame(animate);
+    }
+    animate();
+  
+    
+/* 
+     context.beginPath();
     context.moveTo(100, 100);
     context.lineTo(300, 100);
     context.lineTo(300, 200);
     context.closePath();
-    context.stroke(); */
+    context.stroke(); */ 
 
-    // variables
-    let painting = false;
+    // variables 
+   /*  let painting = false;
 
     function startPosition(e) {
         painting = true;
@@ -39,5 +75,5 @@ window.addEventListener('load', () => {
     //EventListeners
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("mouseup", finishedPosition);
-    canvas.addEventListener("mousemove", draw);
-});
+    canvas.addEventListener("mousemove", draw); */
+/* }); */
